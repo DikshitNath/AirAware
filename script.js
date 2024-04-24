d6541d6601ffc891df952acd519ee5290bb5da33
Dikshit Nath <143931467+DikshitNath@users.noreply.github.com>
2024-04-24 23:11:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+gsap.from("#nav a",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1,
+  stagger: 0.2
+})
+gsap.from("button",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1
+
+})
+
+gsap.from("h1",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1
+
+})
+gsap.from("h6",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1
+
+})
+gsap.from("#image",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1
+
+})
+gsap.from("#logo",{
+  y: -100,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1
+
+})
+
